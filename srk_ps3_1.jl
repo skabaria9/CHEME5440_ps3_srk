7f832ab6559fc31b25f714c2165f36fc990dd9c6
skabaria9 <47298326+skabaria9@users.noreply.github.com>
2019-03-11 15:51:19 -0400

    Add files via upload

--- srk_ps3_1.jl
+++ srk_ps3_1.jl
@@ -60,7 +60,7 @@ stoichiometric_matrix_balanced = [ #v1 v2 v3 v4 v5,1 v5,-1 b1 b2 b3 b4 b5 b6 b7 
 0  0  0  0 -1.5  1.5  0 0  0  0 0  0   0 0  0  0  1  0  0  0  0  0  0 -1  0  0 0; #NADP+
 0  0  0  0 -1    1    0 0  0  0 0  0   0 0  0  1  0  0  0  0  0  0 -1  0  0  0 0; #Nitric oxide
 0  0  0  0  1.5 -1.5  0 0  0  0 0  0   0 0  0  0  0  0  0 -1  0  1  0  0  0  0 0; #H+
-0  0  0  0  2   -2    0 0  0  0 0  0   0 0  0  0  0  0  0  0  0  0  0  0  0 -1 1; #O2
+0  0  0  0  2   -2    0 0  0  0 0  0   0 0  0  0  0  0  0  0  0  0  0  0  0 -1 1 #O2
 ];
 
 #Define atom matrix for the original S matrix stoichiometric_matrix_original
@@ -83,20 +83,132 @@ E_balanced = atom_balanced*stoichiometric_matrix_balanced;
 
 #Define Inputs to the flux array
 
-#Lower Bound
-flux_lower_bound = [0;0;0;0;0;0;0;0;0;0];
-
-#Upper bound (math in separate file)
+#Bound definitions in separate file
 include("upper_bound_math.jl")
-flux_upper_bound = [
-v1_upper_bound;
-v2_upper_bound;
-]
 
-#default_bounds_array = [flux_lower_bound flux_upper_bound];
+#Lower Bound
+flux_lower_bound = [
+v_lower_bound_irrev; #v1
+v_lower_bound_irrev; #v2
+v_lower_bound_irrev; #v3
+v_lower_bound_irrev; #v4
+v_lower_bound_irrev; #v5,1
+v_lower_bound_irrev; #v5,-1
+b_lower_bound_irrev; #b1
+b_lower_bound_irrev; #b2
+b_lower_bound_irrev; #b3
+b_lower_bound_irrev; #b4
+b_lower_bound_irrev; #b5
+b_lower_bound_irrev; #b6
+b_lower_bound_irrev; #b7
+b_lower_bound_irrev; #b8
+b_lower_bound_irrev; #b9
+b_lower_bound_irrev; #b10
+b_lower_bound_irrev; #b11
+b_lower_bound_irrev; #b12
+b_lower_bound_irrev; #b13
+b_lower_bound_irrev; #b14
+b_lower_bound_irrev; #b15
+b_lower_bound_irrev; #b16
+b_lower_bound_irrev; #b17
+b_lower_bound_irrev; #b18
+b_lower_bound_irrev; #b19
+b_lower_bound_irrev; #b20
+b_lower_bound_irrev #b21
+];
+
+flux_upper_bound = [
+v1_upper_bound; #v1
+v2_upper_bound; #v2
+v3_upper_bound; #v3
+v4_upper_bound; #v4
+v5pos1_upper_bound; #v5,1
+v5neg1_upper_bound; #v5,-1
+b_upper_bound; #b1
+b_upper_bound; #b2
+b_upper_bound; #b3
+b_upper_bound; #b4
+b_upper_bound; #b5
+b_upper_bound; #b6
+b_upper_bound; #b7
+b_upper_bound; #b8
+b_upper_bound; #b9
+b_upper_bound; #b10
+b_upper_bound; #b11
+b_upper_bound; #b12
+b_upper_bound; #b13
+b_upper_bound; #b14
+b_upper_bound; #b15
+b_upper_bound; #b16
+b_upper_bound; #b17
+b_upper_bound; #b18
+b_upper_bound; #b19
+b_upper_bound; #b20
+b_upper_bound; #b21
+];
+
+#Compile Bounds
+default_bounds_array = [flux_lower_bound flux_upper_bound];
 
 #Species Bounds Array
-species_bounds_array = [0;0;0;0;0;0;0;0;0;0];
+species_bounds_array = [
+0.0 0.0; #Aspartate
+0.0 0.0; #Arginosuccitate
+0.0 0.0; #Fumarate
+0.0 0.0; #Arginine
+0.0 0.0; #Urea
+0.0 0.0; #Orinithine
+0.0 0.0; #Carbamoyl Phosphate
+0.0 0.0; #Citruline
+0.0 0.0; #ATP
+0.0 0.0; #AMP
+0.0 0.0; #PPi
+0.0 0.0; #H20
+0.0 0.0; #phosphate
+0.0 0.0; #NADPH
+0.0 0.0; #NADP+
+0.0 0.0; #Nitric oxide
+0.0 0.0; #H+
+0.0 0.0; #O2
+];
 
 #Objective Coefficient Array
-objective_coefficient_array = [0;0;0;0;0;0;0;0;0;1];
+objective_coefficient_array =[
+0.0; #v1
+0.0; #v2
+0.0; #v3
+0.0; #v4
+0.0; #v5,1
+0.0; #v5,-1
+0.0; #b1
+0.0; #b2
+0.0; #b3
+-1.0; #b4
+0.0; #b5
+0.0; #b6
+0.0; #b7
+0.0; #b8
+0.0; #b9
+0.0; #b10
+0.0; #b11
+0.0; #b12
+0.0; #b13
+0.0; #b14
+0.0; #b15
+0.0; #b16
+0.0; #b17
+0.0; #b18
+0.0; #b19
+0.0; #b20
+0.0; #b21
+];
+
+#Run the flux function
+flux_answer = calculate_optimal_flux_distribution(stoichiometric_matrix_balanced,default_bounds_array,species_bounds_array,objective_coefficient_array)
+#Pull out the answers
+objective_value= flux_answer[1] #umol/gDW per second
+objective_value_converted = objective_value/1000*3600 #mmol/gDW per hour
+calculated_flux_array= flux_answer[2] #umol/gDW per second
+calculated_flux_array_converted = calculated_flux_array/1000*3600 #mmol/gDW per hour
+dual_value_array = flux_answer[3]
+uptake_array = flux_answer[4]
--- upper_bound_math.jl
+++ upper_bound_math.jl
@@ -11,8 +11,8 @@ metabolite_concentration_ATP = convert_factor(2.25E-3 * 10^6); #ATP
 metabolite_concentration_Fumarate = convert_factor(2.88E-4 * 10^6); #Fumarate, for E. coli (don't need it)
 metabolite_concentration_Arginine = convert_factor(2.55e-4 * 10^6); #Arginine
 metabolite_concentration_Orinithine = convert_factor(4490);#Ornithine
-metabolite_concentration_NADPH = convert_factor(100E-2 * 10^6); #Aspartate
-
+metabolite_concentration_NADPH = convert_factor(100E-2 * 10^6); #NADPH
+metabolite_concentration_Carbamoyl = 1000; #placehoder, Carbamoyl Phosphate, concentration not available
 
 #Km  -- uM converted to umol/gDW
 Km_v1_Aspartate = convert_factor(0.18e-3 * 10^6); #v1, 6.3.4.5, Aspartate (BRENDA Mus musculus)
@@ -24,19 +24,31 @@ Km_v4_Ornithine = convert_factor(0.8e-3 * 10^6); #v4, 2.1.3.3, Ornithine, (BREND
 Km_v4_Carbamoyl = convert_factor(0.13e-3 * 10^6); #v4, 2.1.3.3, Carbamoyl Phosphate, (BRENDA, Homo sapiens, wild type)
 Km_v5_neg1_Arginine = convert_factor(3.50E-6 * 10^6); #v5,-1, 1.14.14.39, Arginine
 Km_v5_neg1_NADPH = convert_factor(3.50E-6 * 10^6); #v5,-1, 1.14.14.39, NADPH
-Km_v5_pos1 = 0; #Assume saturation
+Km_v5_pos1 = 0.0; #Assume saturation, no values available
 
-saturation_term_v1 = 0; #v1, 6.3.4.5
-saturation_term_v2 = 0; #v2, 4.3.2.1
-saturation_term_v3 = 0;#v3, 4.3.2.1
-saturation_term_v4 = 0;#(metabolite_concentration(2)/(Km(5)+metabolite_concentration(2)))*(metabolite_concentration(2)/(Km(5)+metabolite_concentration(2))); #v4, 2.1.3.3
-saturation_term_v5pos1 = 0;#(metabolite_concentration(2)/(Km(3)+metabolite_concentration(2))); #v5,1 1.14.14.39
-saturation_term_v5neg1 = 0;#(metabolite_concentration(2)/(Km(3)+metabolite_concentration(2))); #v5,-1 1.14.14.39
+#Calculate the saturation terms for each
+#If Km or concentration is not available for all or port of the term
+#   that was considered to be the max value = 1; Therefore those parts/terms do not appear in the variable definitions
+saturation_term_v1 = (metabolite_concentration_Aspartate/(Km_v1_Aspartate + metabolite_concentration_Aspartate)) * (metabolite_concentration_ATP/(Km_v1_ATP+metabolite_concentration_ATP)) *(metabolite_concentration_Citruline/(Km_v1_Citruline + metabolite_concentration_Citruline)); #v1, 6.3.4.5
+saturation_term_v2 = 1.0; #v2, 4.3.2.1 (no concentration Arginosuccitate available, assume saturation)
+saturation_term_v3 = (metabolite_concentration_Arginine/(Km_v3_Arginine+metabolite_concentration_Arginine));#v3, 4.3.2.1
+saturation_term_v4 = (metabolite_concentration_Orinithine/(Km_v4_Ornithine+metabolite_concentration_Orinithine))# * (metabolite_concentration_Carbamoyl/(Km_v4_Carbamoyl+metabolite_concentration_Carbamoyl)) (concentration CP not available) #v4, 2.1.3.3
+saturation_term_v5pos1 = 1.0; #v5,1 1.14.14.39 (no Km available, assume saturation)
+saturation_term_v5neg1 = (metabolite_concentration_Arginine/(Km_v5_neg1_Arginine+metabolite_concentration_Arginine))*(metabolite_concentration_NADPH/(Km_v5_neg1_NADPH+metabolite_concentration_NADPH)); ##v5,-1 1.14.14.39 (Km)
 
-
+#Calculate the maximum values of each of the
 v1_upper_bound = k_cat[1]*E*theta*saturation_term_v1;#v1, 6.3.4.5,
 v2_upper_bound = k_cat[2]*E*theta*saturation_term_v2;#v2, 4.3.2.1
 v3_upper_bound = k_cat[3]*E*theta*saturation_term_v3;#v3, 3.5.3.1
 v4_upper_bound = k_cat[4]*E*theta*saturation_term_v4; #v4, 2.1.3.3
+#For v5, assume the kcat is the same in forwards/backwards directions
 v5pos1_upper_bound = k_cat[5]*E*theta*saturation_term_v5pos1;#v5,1 forwards, 1.14.14.39
-v5neg1_upper_bound = k_cat[5]*E*theta*saturation_term_v2;#v5,-1, backwards 1.14.14.39
+v5neg1_upper_bound = k_cat[5]*E*theta*saturation_term_v5neg1;#v5,-1, backwards 1.14.14.39
+
+#Set the v lower bound
+v_lower_bound_irrev = 0.0; #Assume all reactions are irreversible since v5 was split into forwards/backwards
+
+#Set the b bounds
+b_upper_bound = 10.0e3/3600; #s^-1 (given in problem)
+b_lower_bound_irrev = 0.0; #s^-1 (if you assume the reaction is irreversible, used for anything that is only a substrate/product)
+b_lower_bound_rev = -10.0e3/3600; ##For v5, assume the kcat is the same in forwards/backwards directions
